81191def2c5fe8345f0b4510eec500455e17c573
Shriramana Sharma <samjnaa@gmail.com>
2018-08-09 10:28:39 +0530

    consolidate duplicated common parts of into a pure virtual base class

--- src/Kaleidoscope-LEDEffect-Rainbow.cpp
+++ src/Kaleidoscope-LEDEffect-Rainbow.cpp
@@ -1,11 +1,12 @@
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
-#define UPDATE_INTERVAL 50  // milliseconds between two LED updates to avoid overloading; 20 fps
+#include "LEDUtils.h"
 
 namespace kaleidoscope {
 
-void LEDRainbowEffect::update(void) {
-  uint16_t now = millis();
+void LEDRainbowEffectBase::update(void) {
+  now = millis();
   if ((now - last_hue_update) < hue_update_interval) {
+#define UPDATE_INTERVAL 50  // milliseconds between two LED updates to avoid overloading; 20 fps
     if (!breathe || (now - last_update) < UPDATE_INTERVAL) {  // no updates either due to hue change or breathing
       return;
     }
@@ -14,27 +15,17 @@ void LEDRainbowEffect::update(void) {
     last_hue_update = now;
   }
   last_update = now;
+  update_helper();
+}
 
+void LEDRainbowEffect::update_helper(void) {
   cRGB rainbow = breathe ?
                  breath_compute_helper(hue, saturation, now) :
                  hsvToRgb(hue, saturation, brightness);
   ::LEDControl.set_all_leds_to(rainbow);
 }
 
-// ---------
-
-void LEDRainbowWaveEffect::update(void) {
-  uint16_t now = millis();
-  if ((now - last_hue_update) < hue_update_interval) {
-    if (!breathe || (now - last_update) < UPDATE_INTERVAL) {  // no updates either due to hue change or breathing
-      return;
-    }
-  } else {
-    hue += hue_steps;
-    last_hue_update = now;
-  }
-  last_update = now;
-
+void LEDRainbowWaveEffect::update_helper(void) {
   for (uint8_t i = 0; i < LED_COUNT; i++) {
     uint8_t key_hue = hue + 16 * (i / 4);
     cRGB rainbow = breathe ?
@@ -44,20 +35,7 @@ void LEDRainbowWaveEffect::update(void) {
   }
 }
 
-// ---------
-
-void LEDRainbowCheckerboardEffect::update(void) {
-  uint16_t now = millis();
-  if ((now - last_hue_update) < hue_update_interval) {
-    if (!breathe || (now - last_update) < UPDATE_INTERVAL) {  // no updates either due to hue change or breathing
-      return;
-    }
-  } else {
-    hue += hue_steps;
-    last_hue_update = now;
-  }
-  last_update = now;
-
+void LEDRainbowCheckerboardEffect::update_helper(void) {
   for (uint8_t i = 0; i < LED_COUNT; i++) {
     uint8_t key_hue = hue + 4 * (i / 2) + 128 * (i % 2);
     cRGB rainbow = breathe ?
--- src/Kaleidoscope-LEDEffect-Rainbow.h
+++ src/Kaleidoscope-LEDEffect-Rainbow.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "Kaleidoscope-LEDControl.h"
-#include "LEDUtils.h"
 
 namespace kaleidoscope {
-class LEDRainbowEffect : public LEDMode {
+
+class LEDRainbowEffectBase : public LEDMode {
  public:
-  LEDRainbowEffect(void) {}
+  LEDRainbowEffectBase(void) {}
   void update(void) final;
 
   int8_t hue_steps = 1;
@@ -15,46 +15,29 @@ class LEDRainbowEffect : public LEDMode {
   bool breathe = false;
   uint8_t brightness = 50;
 
- private:
+ protected:
+  virtual void update_helper(void) = 0;
   uint8_t hue = 0;
+  uint16_t now;
+
+ private:
   uint16_t last_hue_update = 0;
   uint16_t last_update = 0;
 };
 
-
-class LEDRainbowWaveEffect : public LEDMode {
- public:
-  LEDRainbowWaveEffect(void) {}
-  void update(void) final;
-
-  int8_t hue_steps = 1;
-  uint8_t hue_update_interval = 40;
-  uint8_t saturation = 255;
-  bool breathe = false;
-  uint8_t brightness = 50;
-
- private:
-  uint8_t hue = 0;
-  uint16_t last_hue_update = 0;
-  uint16_t last_update = 0;
+class LEDRainbowEffect : public LEDRainbowEffectBase {
+ protected:
+  virtual void update_helper(void) final;
 };
 
+class LEDRainbowWaveEffect : public LEDRainbowEffectBase {
+ protected:
+  virtual void update_helper(void) final;
+};
 
-class LEDRainbowCheckerboardEffect : public LEDMode {
- public:
-  LEDRainbowCheckerboardEffect(void) {}
-  void update(void) final;
-
-  int8_t hue_steps = 1;
-  uint8_t hue_update_interval = 40;
-  uint8_t saturation = 255;
-  bool breathe = false;
-  uint8_t brightness = 50;
-
- private:
-  uint8_t hue = 0;
-  uint16_t last_hue_update = 0;
-  uint16_t last_update = 0;
+class LEDRainbowCheckerboardEffect : public LEDRainbowEffectBase {
+ protected:
+  virtual void update_helper(void) final;
 };
 }
 
